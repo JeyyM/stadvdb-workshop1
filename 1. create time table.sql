66a4c54fa44f61b7070476f9fe503af027df0095
JeyM <jeymson8000@gmail.com>
2025-09-27 17:38:49 +0800

    datefix

--- 1. create time table.sql
+++ 1. create time table.sql
@@ -1,14 +1,9 @@
-
 -- original schema's tripID is not unique so I'll insert the data as is, no foreign key integrity
 DROP TABLE IF EXISTS date_dimension;
 CREATE TABLE date_dimension (
   timeID INT AUTO_INCREMENT PRIMARY KEY,
   tripID VARCHAR(45) NOT NULL,
-
-  pickup_year  INT,
-  pickup_month INT,
-  pickup_day   INT,
-  dropoff_year  INT,
-  dropoff_month INT,
-  dropoff_day   INT
+  tripYear INT,
+  tripMonth INT,
+  tripDay INT
 ) ENGINE=InnoDB;
