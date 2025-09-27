0877dc71f1d76db0972e3e0e25736995622bc7e2
JeyM <jeymson8000@gmail.com>
2025-09-27 18:30:34 +0800

    changed date

--- 4. trips per vendor.sql
+++ 4. trips per vendor.sql
@@ -2,19 +2,21 @@
 SELECT 
     r.VendorID,
     r.PULocationID,
-    YEAR(STR_TO_DATE(r.tpep_pickup_datetime, '%Y-%m-%d %H:%i:%s')) AS trip_year,
-    MONTH(STR_TO_DATE(r.tpep_pickup_datetime, '%Y-%m-%d %H:%i:%s')) AS trip_month,
+    dd.tripYear AS trip_year,
+    dd.tripMonth AS trip_month,
     COUNT(*) AS trip_count
 FROM taxi_route_details r
-GROUP BY r.VendorID, r.PULocationID, trip_year, trip_month
-ORDER BY trip_year, trip_month, r.VendorID, r.PULocationID;
+JOIN date_dimension dd ON r.tripID = dd.tripID
+GROUP BY r.VendorID, r.PULocationID, dd.tripYear, dd.tripMonth
+ORDER BY dd.tripYear, dd.tripMonth, r.VendorID, r.PULocationID;
 
 -- Count the number of trips per vendor per month per pickup location (OPTIMIZED)
 SELECT 
     r.VendorID,
     r.PULocationID,
-    DATE_FORMAT(STR_TO_DATE(r.tpep_pickup_datetime, '%Y-%m-%d %H:%i:%s'), '%Y-%m') AS trip_month,
+    CONCAT(dd.tripYear, '-', LPAD(dd.tripMonth, 2, '0')) AS trip_month,
     COUNT(*) AS trip_count
 FROM taxi_route_details r
-GROUP BY r.VendorID, r.PULocationID, trip_month
-ORDER BY trip_month, r.VendorID, r.PULocationID;
+JOIN date_dimension dd ON r.tripID = dd.tripID
+GROUP BY r.VendorID, r.PULocationID, dd.tripYear, dd.tripMonth
+ORDER BY dd.tripYear, dd.tripMonth, r.VendorID, r.PULocationID;
