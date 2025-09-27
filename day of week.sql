bbc23f9a9734001ea2946e6188c837b290a7610d
JeyM <jeymson8000@gmail.com>
2025-09-27 12:50:40 +0800

    2 interesting questions

---
+++ day of week.sql
@@ -0,0 +1,7 @@
+-- Goal: Find which day of the week has the most number of trips
+SELECT 
+    DAYNAME(STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p')) AS weekdays,
+    COUNT(*) AS trip_count
+FROM taxi_schema.taxi_route_details
+GROUP BY DAYNAME(STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p'))
+ORDER BY trip_count DESC
